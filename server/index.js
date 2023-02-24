6ae5af7b8e962b166b2c032c7c097f635f811f17
Jason Green <jgreen44@asu.edu>
2023-02-24 17:06:53 -0600

    wip...what the hell am i doing?

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json("Hello World!");
+app.get('/', (req, res) => {
+  res.json('Hello World!');
 });
 
 app.listen(PORT, () => {
